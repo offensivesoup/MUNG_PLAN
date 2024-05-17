baff80fa5629f3939702f244add01f6ce5a4c670
이승지 <esg0107@pusan.ac.kr>
2024-05-17 17:51:15 +0900

    기능 구현: 페이지네이션 진행중

--- final-pjt-front/mungplan-front-project/src/components/life/AdoptList.vue
+++ final-pjt-front/mungplan-front-project/src/components/life/AdoptList.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="adopt-list">
     <AdoptListItem
-      v-for="dog in store.shelterDogs"
+      v-for="dog in displayedPosts"
       :key="dog.id"
       :dog="dog"
     />
   </div>
+  <div>
+    <button v-if="currentPage > 1" @click="currentPage--">이전페이지</button>
+    <span>{{ currentPage }} / {{ totalPages }}</span>
+    <button v-if="currentPage < totalPages" @click="currentPage++">다음페이지</button>
+  </div>
 </template>
 
 <script setup>
+import { ref, computed } from 'vue'
 import { useAdoptStore } from '@/stores/adopt'
 import AdoptListItem from './AdoptListItem.vue'
 
 const store = useAdoptStore()
+
+const currentPage = ref(1)
+const postsPerPage = 10
+
+const displayedPosts = computed(() => {
+  const startIndex = (currentPage.value - 1) * postsPerPage
+  const endIndex = startIndex + postsPerPage
+  return store.shelterDog.slice(startIndex, endIndex)
+})
+
+const totalPages = computed(() => {
+  return Math.ceil(store.shelterDog.length / postsPerPage)
+})
 </script>
 
 <style scoped>
